022529e7e8b4cdedb3890257b00558fc6d69a48b
389cadcom <389cadcom@163.com>
2019-12-25 17:44:10 +0800

    说明

--- z-tip.js
+++ z-tip.js
@@ -1,10 +1,30 @@
 //2019-React 内容
-1.router4(history--自定义)  connected-react-router
+1.router4(history--createBrowserHistory())  connected-react-router
 
 2.redux, react-redux, react-promise, react-saga
 
 3.antd组件内容
 
 4.路由异步加载   react-loadable, AsyncComponent
+  // AsyncComponent(() => import('./home'))
+  /* const Home = Loadable({
+    loader: () => import("./containers/Home"),
+    loading: MyLoadingComponent
+  }); */
 
 4.Decorator  全局sass样式  sass-resources-loader
+
+
+//通用的过场组件
+const MyLoadingComponent = ({isLoading, error}) => {
+  // Handle the loading state
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  else if (error) {
+    return <div>Sorry, there was a problem loading the page.</div>;
+  }
+  else {
+    return null;
+  }
+}
